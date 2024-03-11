34e997bf73d17af663ee59cecbed7c0ce9266b4a
user <user@WIN-2MSGKLHNS9A>
2024-03-11 18:25:39 +0300

    Добавьте файлы проекта.

---
+++ Lab17/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class BankAccount<T>
+    {
+        private T accountNum { get; set; }
+
+        private int Summa { get; set; }
+
+        private string Name { get; set; }
+
+
+        public BankAccount(T num, int summa, string name)
+        {
+            accountNum = num;
+            Name = name;
+            Summa = summa;
+        }
+
+        public string GetInfo()
+        {
+            return $"{accountNum} {Summa} {Name}";
+        }
+
+        //public void Input()
+        //{
+        //    Console.WriteLine("Введите номер счета");
+        //    accountNum = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+        //    Console.WriteLine("Введите баланс");
+        //    Summa = Convert.ToInt32(Console.ReadLine());
+        //    Console.WriteLine("Введите ФИО");
+        //    Name = Console.ReadLine();
+
+        //}
+
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер счета");
+            int numint = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите баланс");
+            int summa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите фИО");
+            string name = Console.ReadLine();
+            BankAccount<int> bankAccount1 = new BankAccount<int>(numint, summa, name);
+            Console.WriteLine(bankAccount1.GetInfo());
+
+            Console.WriteLine("Введите номер счета");
+            string numString = Console.ReadLine();
+            Console.WriteLine("Введите баланс");
+            summa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите фИО");
+            name = Console.ReadLine();
+
+            BankAccount<string> bankAccount2 = new BankAccount<string>(numString, summa, name);
+            Console.WriteLine(bankAccount2.GetInfo());
+
+            //BankAccount<int> bankAccount1 = new BankAccount<int>();
+            //bankAccount1.Input();
+            //Console.WriteLine(bankAccount1.GetInfo());
+
+            //BankAccount<string> bankAccount2 = new BankAccount<string>();
+            //bankAccount2.Input();
+            //Console.WriteLine(bankAccount2.GetInfo());
+
+            Console.ReadKey();
+        }
+    }
+}
